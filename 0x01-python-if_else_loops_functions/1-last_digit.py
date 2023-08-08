f25722f3c4d30401bdee775904fb591d67499078
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-08-08 15:02:47 +0100

    convert 0-positive file to exe

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
-val = random.randint(-10000, 10000)
-amt = abs(val) % 10
-
-if val < 0:
-    amt = -amt
-    print("Last amt of {} is {} and is ".format(val, amt), end="")
-    if amt > 5:
-        print("greater than 5")
-    elif amt == 0:
-        print("0")
+number = random.randint(-10000, 10000)
+if number >= 0:
+    last_digit = number % 10
+else:
+    last_digit = ((number * -1) % 10) * -1
+    output = "Last digit of %d is %d and is" % (number, last_digit)
+    if last_digit == 0:
+        print(output, "0")
+    elif last_digit < 6:
+        print(output, "less than 6 and not 0")
     else:
-        print("less than 6 and not 0")
+        print(output, "greater than 5")

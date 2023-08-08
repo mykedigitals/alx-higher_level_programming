66b3dee1c847966a67d855001e5f5a6cdc92fea2
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-08-08 15:57:26 +0100

    debugging to fix things

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
 
-"""Print the alphabet in lowercase,
-not followed by a new line."""
+"""Print the alphabet in lowercase, not followed by a new line."""
 
 for alpha in range(97, 123):
-        print("{}".format(chr(alpha)), end="")
-
+    print("{:c}".format(character), end="")

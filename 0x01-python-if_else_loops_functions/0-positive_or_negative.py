3011d21c6f8cd6d6ea4c3233050a463427474d58
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-08-08 14:45:36 +0100

    convert 0-positive file to exe

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,9 +5,7 @@ val = random.randint(-10, 10)
 
 if val > 0:
     print("{} is positive".format(val))
-
+elif val < 0:
+    print("{} is negative".format(val))
 elif val == 0:
     print("{} is zero".format(val))
-
-else:
-    print("{} is negative".format(val))

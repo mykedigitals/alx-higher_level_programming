380f8015b526a7f7b33fc8264604eeac5fba16b9
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-08-09 14:31:42 +0100

    trying to push to remote, but got an error

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,6 @@
 def print_last_digit(value):
     if value < 0:
         value *= -1
-        result = value % 10
-        print('{}'.format(result), end="")
-        return result
+    result = value % 10
+    print('{}'.format(result), end="")
+    return result

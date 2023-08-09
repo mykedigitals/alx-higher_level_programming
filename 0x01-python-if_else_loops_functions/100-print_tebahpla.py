e5db343b03f254fbb22c318bb4c80a226b242a32
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-08-09 17:27:33 +0100

    fixing bugs

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-""""Print the alphabet in reverse order alternating upper- and lower-case."""
+""""Print the alphabet in reverse order
+alternating upper- and lower-case."""
 
 val = 0
 for num in range(ord('z'), ord('a') - 1, -1):

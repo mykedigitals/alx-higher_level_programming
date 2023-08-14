1a21cbc2be83184be00c55bbada66e97a8993a12
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-08-14 11:09:07 +0100

    A function that removes all characters c and C from a string

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end="" if j == i[-1] else " ")
-        print()
+    for pik in range(len(matrix)):
+        for trik in range(len(matrix[pik])):
+            print("{:d}".format(matrix[pik][trik]), end="")
+            if trik != len(matrix[pik]) -1):
+            print(" ", end="")
+        print("")

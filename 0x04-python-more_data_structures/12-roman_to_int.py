798ce64fa691de0a5885cfddb6d41cc3635f038a
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-08-17 10:11:46 +0100

    Write a function that computes the square value of all integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,27 +3,24 @@
 
 
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-    roman_dict = {
+    romans = {
             "I": 1,
             "V": 5,
             "X": 10,
             "L": 50,
             "C": 100,
             "D": 500,
-            "M": 1000
-            }
-    num = 0
-
+            "M": 100}
+    total = 0
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    if len(roman_string) == 1:
+        return romans.get(roman_string)
     for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
-            else:
-                num += roman_dict[roman_string[i]]
-    return (num)
+        if i + 1 == len(roman_string):
+            total += romans.get(roman_string[i])
+        elif romans.get(roman_string[i]) >= romans.get(roman_string[i + 1]):
+            total += romans.get(roman_string[i])
+        else:
+            total -= romans.get(roman_string[i])
+    return total

e94d497525344a6658ecb365c224eec2de1a93ab
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-08-17 09:54:38 +0100

    Write a function that computes the square value of all integers

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,13 +2,6 @@
 
 
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None
-
-    bet = list(a_dictionary.keys())[0]
-    huge = a_dictionary[bet]
-    for r, v in a_dictionary.items():
-        if g > huge:
-            huge = g
-            bet = r
-    return (bet)
+    if not a_dictionary:
+        return (None)
+    return (max(a_dictionary, key=a_dictionary.get))

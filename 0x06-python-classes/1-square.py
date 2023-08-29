b020b8e52bbf8ad4933a470f367efee0487508c1
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-08-29 13:51:58 +0100

    class and objects Task one to five

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class called Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize data.
+
+        Args:
+            size: The size of the new square.
+        """
+        self.__size = size

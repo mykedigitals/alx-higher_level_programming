9b8316d33402d8f077b018f7e89a523cf122674f
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-11-20 10:40:39 +0100

    first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa
 """
-from sys import argv
-import MySQLdb
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
 
+import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
-    user, password, database = argv[1], argv[2], argv[3]
-    db = MySQLdb.connect(host="localhost", user=user,
-                         passwd=password, db=database)
-    db.query("SELECT * FROM states ORDER BY id")
-    r = db.store_result()
-    t = r.fetch_row(maxrows=0)
-    for i in t:
-        print(i)
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute("SELECT * FROM states")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
